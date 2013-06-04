964326a91716f2c14550fb2575413a39f7445fbb
peji <peji@Chevron>
2013-06-05 01:46:39 +0430

    multi threading used to increase performance

--- src/tsp/TSP.java
+++ src/tsp/TSP.java
@@ -10,7 +10,12 @@ package tsp;
  */
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * A solution that solves TSP problem
@@ -32,44 +37,121 @@ public class TSP {
      */
     static int[][] dpData;
     
-    /**
-     * The method that calculates minimum moves for TSP problem.
-     * Works with O( n^2 * 2^n )
-     * @param start is the vertex to start from
-     * @param verSet is the set of vertices that should be passed
-     * @param end is the vertex should be returned to after traveling all vertices in @param verSet
-     * @return the minimum moves
-     */
-    private static int g( int start, List<Integer> verSet, int end ) {
+    private static class TSPThread implements Runnable {
         
-        int i, min = Integer.MAX_VALUE, buff, elem, dploc = 0;
-        List list;
+        private static int start, end;
+        private static List<Integer> verSet;
+        final int sharedIndex;
+        final int[] sharedData;
         
-        if( verSet.isEmpty() ) {                                                // Checks if there is no vertex to travel to, retruns the distance from "start" to "end"
-            return distances[start][end];
-        }
-        
-        for( i = 0; i < verSet.size(); i++ ) {                                  // Generates the index of result in dpData
-            dploc += Math.pow( 2, start<verSet.get(i)?verSet.get(i)-1:verSet.get(i) ) - 1;
-        }
-        
-        if( dpData[start][dploc] != 0 ) {                                       // Checks if there is data in dploc, and returns it if there is
-            return dpData[start][dploc];
-        }
-        
-        for( i = 0; i < verSet.size(); i++ ) {
-            elem = verSet.get(i);                                               // Gets the ith element from verSet and sets elem to it
-            list = new ArrayList< Integer >();                                  // Makes a new list for next subproblem
-            list.addAll( verSet );                                              // Adds All verSet elements to new list
-            list.remove( list.indexOf( elem ) );                                // Removes elem from new list
-            buff = distances[start][elem] + g( elem, list, end );               // Calculates current subproblem recursively
-            if( buff < min ) {                                                  // Checks if calculated subproblem is less than others set min to it
-                min = buff;
+        TSPThread( int start, List<Integer> verSet, int end, int index, int[] sharedData ) {
+            this.start = start;
+            this.end = end;
+            this.verSet = verSet;
+            this.sharedData = sharedData;
+            this.sharedIndex = index;
+            synchronized( sharedData ) {
+                sharedData.notify();
             }
         }
         
-        dpData[start][dploc] = min;                                             // Saves the result in dpData
+        /**
+         * The method that calculates minimum moves for TSP problem.
+         * Works with O( n^2 * 2^n )
+         * @param start is the vertex to start from
+         * @param verSet is the set of vertices that should be passed
+         * @param end is the vertex should be returned to after traveling all vertices in @param verSet
+         * @return the minimum moves
+         */
+        private static int g( int start, List<Integer> verSet ) {
+
+            int i, min = Integer.MAX_VALUE, buff, elem, dploc = 0;
+            List list;
+
+            if( verSet.isEmpty() ) {                                                // Checks if there is no vertex to travel to, retruns the distance from "start" to "end"
+                return distances[start][end];
+            }
+            
+            for( i = 0; i < verSet.size(); i++ ) {                                  // Generates the index of result in dpData
+                dploc += Math.pow( 2, start<verSet.get(i)?verSet.get(i)-1:verSet.get(i) ) - 1;
+            }
+            
+            if( dpData[start][dploc] != 0 ) {                                       // Checks if there is data in dploc, and returns it if there is
+                return dpData[start][dploc];
+            }
+            
+            for( i = 0; i < verSet.size(); i++ ) {
+                elem = verSet.get(i);                                               // Gets the ith element from verSet and sets elem to it
+                list = new ArrayList< Integer >();                                  // Makes a new list for next subproblem
+                list.addAll( verSet );                                              // Adds All verSet elements to new list
+                list.remove( list.indexOf( elem ) );                                // Removes elem from new list
+                buff = distances[start][elem] + g( elem, list );                    // Calculates current subproblem recursively
+                if( buff < min ) {                                                  // Checks if calculated subproblem is less than others set min to it
+                    min = buff;
+                }
+            }
+
+            dpData[start][dploc] = min;                                             // Saves the result in dpData
+            
+            return min;
+        }
         
+        @Override
+        public void run() {
+            sharedData[ sharedIndex ] += g( start, verSet );
+        }
+        
+    }
+    
+    public static int runThreads( int start, List<Integer> vertices, int end ) {
+        
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        int min = Integer.MAX_VALUE, elem;
+        int[] sharedData = new int[ vertices.size() ];
+        List list;
+
+        if( vertices.isEmpty() ) {                                              // Checks if there is no vertex to travel to, retruns the distance from "start" to "end"
+            return distances[start][end];
+        }
+        
+        for( int i = 0; i < vertices.size(); i++ ) {
+            elem = vertices.get(i);                                             // Gets the ith element from verSet and sets elem to it
+            list = new ArrayList< Integer >();                                  // Makes a new list for next subproblem
+            list.addAll( vertices );                                            // Adds All verSet elements to new list
+            list.remove( list.indexOf( elem ) );                                // Removes elem from new list
+            sharedData[i] = distances[start][elem];
+            executor.execute( new TSPThread( elem, list, end, i, sharedData ) );
+           /* try {
+                synchronized( sharedData ) {
+                    sharedData.wait();
+                }
+            
+            } catch (InterruptedException ex) {
+                Logger.getLogger(TSP.class.getName()).log(Level.SEVERE, null, ex);
+            }*/
+            //    /*
+                try {
+                    Thread.sleep(20);
+                } catch (InterruptedException ex) {
+                    System.out.println( ex.getMessage() );
+                }
+               // */
+        }
+        
+        executor.shutdown();
+        
+        try {
+            while( ! executor.isTerminated() )
+                Thread.sleep(200);
+        } catch (InterruptedException ex) {
+            System.out.println( ex.getMessage() );
+        }
+        
+        for( int i = 0; i < sharedData.length; i++ ) {
+            min = sharedData[i] < min ? sharedData[i] : min;
+        }
+
         return min;
     }
     
@@ -77,7 +159,8 @@ public class TSP {
         
         Scanner input = new Scanner( System.in );
         
-        int i, j, start, end;
+        int i, j, start;
+        
         List< Integer > vertices = new ArrayList< Integer >();
         
         System.out.println("Please Enter vertice count : ");
@@ -89,24 +172,19 @@ public class TSP {
         for( i = 0; i < verCount; i++ ) {                                       // Generates vertices array
             vertices.add( i );
         }
-        
+
         System.out.println("Please Enter distances matrix : ");
         
         for( i = 0; i < verCount; i++ ) {                                       // Gets distances from stdio
-            
             for( j = 0; j < verCount; j++ ) {
                 distances[i][j] = input.nextInt();
             }
-            
         }
         
         System.out.println("Please Enter Start Vertex : ");
-        
         start = input.nextInt();                                                // Gets start vertex from stdio
-        
         vertices.remove( start );                                               // Removes start vertex from vertices list
-        
-        System.out.println( "Min move : " + g( start, vertices, start ) );      // Calculates and prints the result
+        System.out.println( "Min move : " + runThreads( start, vertices, start ) );// Calculates and prints the result
         
     }
 }
